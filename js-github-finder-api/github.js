56ce5478433369d81ac4e750b2a5ea9b0629c275
Dita Pesek <33196424+DeltaForce14@users.noreply.github.com>
2020-06-29 15:51:11 +0100

    Update github.js

--- js-github-finder-api/github.js
+++ js-github-finder-api/github.js
@@ -5,8 +5,6 @@ class Github {
     // this.repos_sort = 'created: asc'; used to sort repos from latest down
     constructor() {
       this.repos_count = 5;
-      this.client_id = '2fae3fabedd4facb646a';
-      this.client_secret = '73c40d007ab630bf5c237cc833c43e225e5154da';
       this.repos_sort = 'created: asc';
     }
   
@@ -17,7 +15,7 @@ class Github {
     `https://api.github.com/users/${user}`, 
     {
       headers: {
-        authorization: "token 7f007f75ef7dfabe4f051ace71f79da19ea7a933"
+        authorization: "token "
       }
     }
   )
@@ -25,7 +23,7 @@ class Github {
     `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
     {
       headers: {
-        authorization: "token 7f007f75ef7dfabe4f051ace71f79da19ea7a933"
+        authorization: "token "
       }
     }
   )
